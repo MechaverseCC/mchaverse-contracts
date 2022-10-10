ba241006b21a406a027e3582521c61721f76a585
MechaverseCC <108651042+MechaverseCC@users.noreply.github.com>
2022-10-10 12:38:51 +0800

    Update MM.sol

--- contracts/token/MM.sol
+++ contracts/token/MM.sol
@@ -1,44 +1,11 @@
 //SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
+pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 
-
-contract MM is ERC20("MetaMecha", "MM"), AccessControl, ERC20Burnable {
-
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
-    modifier onlyGovernor() {
-        require(
-            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
-            "MM: caller is not the governor"
-        );
-        _;
-    }
-
-    modifier onlyMinter() {
-        require(
-            hasRole(MINTER_ROLE, msg.sender),
-            "MM: caller is not the minter"
-        );
-        _;
-    }
+contract MM is ERC20("MetaMecha", "MM") {
     constructor() {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    }
-
-    function grantMinter(address minter) public onlyGovernor {  
-        _setupRole(MINTER_ROLE, minter);
-    }
-
-    function revokeMinter(address minter) public onlyGovernor {
-        revokeRole(MINTER_ROLE, minter);
-    }
-
-    function mint (address to, uint256 amount) public onlyMinter {
-        _mint(to, amount);
+        _mint(msg.sender,20e26);
     }
 
 }
